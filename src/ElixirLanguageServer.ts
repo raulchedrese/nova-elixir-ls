9178ad79458e081c45e96743d5e5c8f6159efa2f
abdul hamid <abdulachik@gmail.com>
2022-02-02 12:57:28 -0600

    saving this

--- src/ElixirLanguageServer.ts
+++ src/ElixirLanguageServer.ts
@@ -25,7 +25,7 @@ export default class ElixirLanguageServer {
   start(path: string) {
     if (this.languageClient) {
       this.languageClient.stop();
-      nova.subscriptions.remove(this.languageClient as unknown as Disposable);
+      nova.subscriptions.remove(this.languageClient);
     }
 
     makeServerExecutable();
@@ -38,7 +38,7 @@ export default class ElixirLanguageServer {
       client.start();
 
       // Add the client to the subscriptions to be cleaned up
-      nova.subscriptions.add(client as unknown as Disposable);
+      nova.subscriptions.add(client);
       this.languageClient = client;
 
       sendDidChangeConfigurationNotification(client, nova.config);
@@ -56,7 +56,7 @@ export default class ElixirLanguageServer {
         handleAddTextEditor(
           this.mainDisposable,
           client,
-          nova.config.get("elixir-ls.formatOnSave") === 'true'
+          Boolean(nova.config.get("elixir-ls.formatOnSave"))
         )
       );
     } catch (err) {
@@ -90,7 +90,7 @@ export default class ElixirLanguageServer {
   stop() {
     if (this.languageClient) {
       this.languageClient.stop();
-      nova.subscriptions.remove(this.languageClient as unknown as Disposable);
+      nova.subscriptions.remove(this.languageClient);
       this.mainDisposable.dispose();
       this.languageClient = null;
     }
--- src/notifications/didChangeConfiguration.ts
+++ src/notifications/didChangeConfiguration.ts
@@ -1,4 +1,26 @@
+interface ElixirLS {
+  dialyzerEnabled: boolean;
+  dialyzerFormat: string;
+  mixEnv: string;
+  projectDir?: string;
+}
+
 export function sendDidChangeConfigurationNotification(client: LanguageClient, novaConfig: Configuration) {
+  let elixirLS: ElixirLS = {
+    dialyzerEnabled: Boolean(novaConfig.get("elixir-ls.dialyzerEnabled")),
+    dialyzerFormat: "dialyzer",
+    mixEnv: String(novaConfig.get("elixir-ls.mixEnv")),
+  }
+  
+  const projectDir = novaConfig.get("elixir-ls.projectDir");
+  
+  if (projectDir) {
+    elixirLS = {
+      ...elixirLS,
+      projectDir: String(projectDir)
+    }
+  }
+  
   client.sendNotification("workspace/didChangeConfiguration", {
     settings: {
       elixirLS: {
