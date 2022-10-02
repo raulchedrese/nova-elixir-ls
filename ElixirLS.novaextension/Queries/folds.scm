90419867edaa9eab1696c97157315e0d251c7e26
Raul Chedrese <raulchedrese@gmail.com>
2022-10-01 18:33:55 -0600

    Improvements to code folding.

--- ElixirLS.novaextension/Queries/folds.scm
+++ ElixirLS.novaextension/Queries/folds.scm
@@ -1,4 +1,25 @@
-(do_block
+(map
+  "{" @start
+  "}" @end
+)
+
+(list
+  "[" @start
+  "]" @end
+)
+
+(call
+  target: (identifier) @keyword
+  (do_block
     "do" @start
     "end" @end
+   (#set! role function)
+   (#set! scope.byLine)
+  )
+  (#match? @keyword "^(def|defp|defmacro|defmacrop|defn|defnp|defprotocol|defimpl)$")
+)
+
+(do_block
+  "do" @start
+  "end" @end
 )
