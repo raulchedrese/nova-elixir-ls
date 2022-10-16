d87d0e7223cd508cfc1b140f5869e0fe955cd10f
Raul Chedrese <raulchedrese@gmail.com>
2022-10-16 14:49:25 -0600

    Fix heex injections.

--- ElixirLS.novaextension/Queries/injections.scm
+++ ElixirLS.novaextension/Queries/injections.scm
@@ -3,5 +3,5 @@
   (sigil_name) @_sigil_name
   (quoted_content) @injection.content)
  (#eq? @_sigil_name "H")
- (#set! injection.language heex)
+ (#set! injection.language "html+eex")
  (#set! injection.combined))
